6935794559415bd8d466619f3358a38a8f0ac370
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-02-03 12:51:39 -0600

    Add files via upload

---
+++ EVA_1_HOLAMUNDO/src/eva_1_holamundo/EVA_1_HOLAMUNDO.java
@@ -0,0 +1,16 @@
+package eva_1_holamundo;
+
+
+public class EVA_1_HOLAMUNDO {
+
+    
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println("x");
+        
+    }
+    public static void algo(){
+        System.out.println("x");
+    }
+    
+}
