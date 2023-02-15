1c3d5c934e2ec702fcba3eb286129da84b692f12
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-02-15 12:44:23 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION.java
@@ -0,0 +1,18 @@
+
+package eva1_4_television;
+
+
+public class EVA1_4_TELEVISION {
+
+   
+    public static void main(String[] args) {
+        tele_imawhipyoass tv1 = new tele_imawhipyoass();
+        tv1.cambiarPower();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+       tv1.subircanal();
+       tv1.bajarcanal();
+        
+    }   
+}
