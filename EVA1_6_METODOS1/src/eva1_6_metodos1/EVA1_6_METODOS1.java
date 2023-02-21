b1eeb95e420df96f8dcc21d4c6f9fdfa9c1e0db2
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-02-21 12:41:03 -0600

    Add files via upload

---
+++ EVA1_6_METODOS1/src/eva1_6_metodos1/EVA1_6_METODOS1.java
@@ -0,0 +1,14 @@
+package eva1_6_metodos1;
+public class EVA1_6_METODOS1 {
+
+    
+  
+    public static void main(String[] args) {
+        myMethod();
+    }
+
+    public static void myMethod(){
+        System.out.println("You have called me ! My name is myMethod");
+        
+    }
+}
