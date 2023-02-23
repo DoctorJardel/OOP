2474f6ba86bacb53c3e95738401fb0dc32f6d9ac
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-02-23 12:19:50 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,17 @@
+package eva1_12_alcance;
+
+
+public class EVA1_12_ALCANCE {
+
+  
+    public static void main(String[] args) {
+        /*System.out.println(x); No existe porque está declarada de la función//
+        La variable, en este caso "x" tiene que estar declarada despues de la funcion*/
+        System.out.println(args);
+        int x = 100;
+        System.out.println(x);
+    }
+    
+    
+    
+}
