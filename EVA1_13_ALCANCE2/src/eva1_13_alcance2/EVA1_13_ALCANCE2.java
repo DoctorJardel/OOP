cada0b67cba21ec7af98e49fe7f7ec6b3797b038
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-02-23 12:25:28 -0600

    Add files via upload

---
+++ EVA1_13_ALCANCE2/src/eva1_13_alcance2/EVA1_13_ALCANCE2.java
@@ -0,0 +1,26 @@
+
+package eva1_13_alcance2;
+
+
+public class EVA1_13_ALCANCE2 {
+
+   
+    public static void main(String[] args) {
+        for (int i = 0; i > 10; i++){
+        System.out.println(i);
+        System.out.println(x);
+        int x = 100;
+        System.out.println(x);
+        {
+        int y = 1000;
+        System.out.println(y);
+        }
+            System.out.println(y);
+    }
+        System.out.println(i);
+        System.out.println(x);
+        
+     
+    }
+    
+}
